83b562e8e3c29a54860788631592bf2c450e345c
Fozan-Developer <nazar.korkin@mail.ru>
2023-02-06 04:06:36 +0300

    update

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ const methods = require('./lib/methods.js');
 const utils = require('./lib/utils.js');
 const connect = require('./lib/methods/connect.js');
 
-module.exports = {
-	connect,
-	methods,
-	utils
-};
+class json_db {
+	constructor(argument) {
+		const { save, models } = argument;
+		if(!save || typeof save != "number") throw new TypeError("Не указано время, или значение не является числом");
+		if(!models || models.length == 0) throw new Error("Не указано поле models или в нём нет ни одной модели");
+		// constructor({save: save, models: models});
+
+		this.save = save;
+		this.models = models;
+		this.active = false;
+
+		connect({save: this.save, models: this.models}).then(x=> this.active = true);
+	};
+
+    async methods(methodName, params = {}) {
+    	setTimeout(async () => {
+    		await methods(methodName, params);
+    	}, this.active == true ? 0 : 3000);
+    };
+
+
+}
+
+module.exports = json_db;
