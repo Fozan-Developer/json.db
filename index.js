17b3ec8c3e2f66057d3d3da0abeddfad8c5a58ed
Fozan-Developer <nazar.korkin@mail.ru>
2023-02-24 01:25:42 +0300

    update module errors

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const checkMainFile = require('./lib/functions/checkMainFile.js')();
 const connect = require('./lib/methods/connect.js');
 const methods_module = require('./lib/methods.js');
 const utils_module = require('./lib/utils.js');
-// const utils_module = require('./lib/functions/models_dir.js');
+const errors = require('./lib/events/errors.js');
 
 class json_db {
 	constructor(argument) {
 		const { save, models, backup } = argument;
-		if(!save || typeof save != "number") throw new TypeError("Не указано время, или значение не является числом");
-		if(!models || models.length == 0) throw new Error("Не указано поле models или в нём нет ни одной модели");
+		if(!save || typeof save != "number") return errors("1.3");
+		if(!models || models.length == 0) return errors("1.4");
 
 		(async () => {
 		this.save = save;
@@ -25,14 +25,14 @@ class json_db {
 	};
 
     async methods(methodName, params = {}) {
-    	if(!methodName) throw new TypeError("Не указано название метода.");
-    	
-    	const method = await methods_module(methodName, params);	
+    	if(!methodName) return errors("1.5");
+
+    	const method = await methods_module(methodName, params);
     	return method;
     };
 
     async utils(methodName, params = {}) {
-    	if(!methodName) throw new TypeError("Не указано название утилиты.");
+    	if(!methodName) return errors("1.6");
 		const util = await utils_module(methodName);
 
     	return util;
